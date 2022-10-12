d252f0aff49b367c12a46e3c7a89548f4a6877c1
cbarbee-capinfogroup <cbarbee@capinfogroup.com>
2022-10-11 23:04:43 -0400

    feat: add custom posttype to widget filter

--- wordpress/wp-content/themes/sydney-child/functions.php
+++ wordpress/wp-content/themes/sydney-child/functions.php
@@ -16,12 +16,33 @@ function sydney_child_enqueue() {
 
 }
 
-/* ADD YOUR CUSTOM FUNCTIONS BELOW */
+/**
+ * Enqueues the Fontawesome from Parent (Sydney) theme
+ *
+ */
+add_action('wp_enqueue_scripts','fontawesome_style_enqueue');
+function fontawesome_style_enqueue(){
+    //TODO: find the admin option to include this globally vs in the code.
+    wp_enqueue_style('fontawesome', get_template_directory_uri() . '/fonts/font-awesome-v5/all.min.css');
+}
+
 
 /**
  * Include custom shortcode here.
  */
 include_once ('custom-shortcodes.php');
+
+/**
+ * Include custom PostType widget here.
+ */
+include_once ('custom-posttype-widget.php');
+
+//filter post_title to append type (if 'book')
+add_filter('the_title', 'wpchad_filter_example', 10, 2);
+function wpchad_filter_example($title ,$id) {
+    //if posttype is 'book'                        go get the publication year here.
+    return (get_post_type($id) == "book") ? '<i class="fas fa-book book-icon"></i>'. $title . ' published ('.get_field( "publication_year", $id , true)['value'].')' : $title ;
+}
 
 /**
  * CHILD - Theme dashboard.
